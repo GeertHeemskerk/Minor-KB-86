7ef8d4516d3d70d9e8065904d2fafe5737fc07ef
Steven Wu <master_msp1@hotmail.com>
2020-06-08 16:39:18 +0200

    start header and homepagina

--- pages/index.vue
+++ pages/index.vue
@@ -1,82 +1,60 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        Minor-KB-86
-      </h1>
-      <h2 class="subtitle">
-        Minor KB-86 van Geert
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          Github
-        </a>
-          <h1>Heading 1</h1>
-          <h2>Heading 2</h2>
-          <h3>Heading 3</h3>
-          <h4>Heading 4</h4>
-          <h5>Heading 5</h5>
-          <h6>Heading 6</h6>
-          <p>Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak met letters 
-am en ze door elkaar husselde om een font-catalogus te maken. Het heeft niet alleen vijf eeuwen overleefd maar is ook, vrijwel onveranderd, overgenomen in elektronische letterzetting. Het is in de jaren '60 populair geworden met de introductie van Letraset vellen met Lorem Ipsum passages en meer recentelijk door desktop publishing software zoals Aldus PageMaker die versies van Lorem Ipsum bevatten.</p>
-      </div>
-    </div>
-  </div>
+  <main class="main">
+    <Header />
+    <section class="content">
+      <h1 class="intro__title">SSDG</h1>
+      <figure class="intro__figure">
+        <img class="intro__media" src="~/assets/images/intro-image.png" alt="verf die samenkomt">
+      </figure>
+    </section>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Header from '~/components/Header.vue';
 export default {
   components: {
-    Logo
+    Header,
   }
 }
 </script>
 
-<style lang="scss">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped lang="scss">
+.main {
+  background-color: black; //TODO: USE VARIABLE
+  width: 100vw;
+  height: 100vh;
 }
+// .container {
+//   margin: 0 auto;
+//   min-height: 100vh;
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   text-align: center;
+// }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-  font-family: $f-heading;
-}
+// .title {
+//   font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+//     'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+//   display: block;
+//   font-weight: 300;
+//   font-size: 100px;
+//   color: #35495e;
+//   letter-spacing: 1px;
+//   font-family: $f-heading;
+// }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-  font-family: $f-heading;
-}
+// .subtitle {
+//   font-weight: 300;
+//   font-size: 42px;
+//   color: #526488;
+//   word-spacing: 5px;
+//   padding-bottom: 15px;
+//   font-family: $f-heading;
+// }
 
-.links {
-  padding-top: 15px;
-}
+// .links {
+//   padding-top: 15px;
+// }
 </style>
